b6e201d63c633cd762c67137fad6980b2d0177da
julienbigras <julien.bigras@hotmail.com>
2019-05-21 00:54:37 -0400

    more media queries, code cleanup, unused assets deleted

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const musicRecommendations = {
             url: "assets/barkley-billy-talent-self-titled.jpg",
             altText: "a red toy poodle named Barkley lying on a couch with Billy Talent's self-titled CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Foo Fighters",
@@ -14,7 +14,7 @@ const musicRecommendations = {
             url: "assets/barkley-foo-fighters-the-color-and-the-shape.jpg",
             altText: "a red toy poodle named Barkley lying on a couch with the Foo Fighters' The Color and the Shape CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Finger Eleven",
@@ -22,7 +22,7 @@ const musicRecommendations = {
             url: "assets/barkley-finger-eleven-five-crooked-lines.jpg",
             altText: "a red toy poodle named Barkley sitting on a chair with Finger Eleven's Five Crooked Lines CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Incubus",
@@ -30,7 +30,7 @@ const musicRecommendations = {
             url: "assets/barkley-incubus-make-yourself.jpg",
             altText: "a red toy poodle named Barkley lying on a bed with Incubus' Make Yourself CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Our Lady Peace",
@@ -38,7 +38,7 @@ const musicRecommendations = {
             url: "assets/barkley-our-lady-peace-happiness.jpg",
             altText: "a red toy poodle named Barkley lying on a hardwood floor with Our Lady Peace's Happiness CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Matthew Good Band",
@@ -46,7 +46,7 @@ const musicRecommendations = {
             url: "assets/barkley-matthew-good-band-beautiful-midnight.jpg",
             altText: "a red toy poodle named Barkley lying on a carpet with Matthew Good Band's Beautiful Midnight CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Queens of the Stone Age",
@@ -54,7 +54,7 @@ const musicRecommendations = {
             url: "assets/barkley-queens-of-the-stone-age-like-clockwork.jpg",
             altText: "a red toy poodle named Barkley sitting on a couch with Queens of the Stone Age's Like Clockwork CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Red Hot Chili Peppers",
@@ -62,7 +62,7 @@ const musicRecommendations = {
             url: "assets/barkley-red-hot-chili-peppers-by-the-way.jpg",
             altText: "a red toy poodle named Barkley lying on a chair with Red Hot Chili Peppers' By the Way CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
     ],
     popPunk: [
@@ -72,7 +72,7 @@ const musicRecommendations = {
             url: "assets/barkley-blink-182-take-off-your-pants-and-jacket.jpg",
             altText: "a red toy poodle named Barkley sitting on a hardwood floor with Blink 182's Take Off Your Pants and Jacket CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Four Year Strong",
@@ -80,7 +80,7 @@ const musicRecommendations = {
             url: "assets/barkley-four-year-strong-explains-it-all.jpg",
             altText: "a red toy poodle named Barkley sitting on a chair with Four Year Strong's Explains It All CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Gob",
@@ -88,7 +88,7 @@ const musicRecommendations = {
             url: "assets/barkley-gob-foot-in-mouth-disease.jpg",
             altText: "a red toy poodle named Barkley lying in the grass with Gob's Foot in Mouth CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Green Day",
@@ -96,7 +96,7 @@ const musicRecommendations = {
             url: "assets/barkley-green-day-american-idiot.jpg",
             altText: "a red toy poodle named Barkley lying in the grass with Green Day's American Idiot CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "New Found Glory",
@@ -104,7 +104,7 @@ const musicRecommendations = {
             url: "assets/barkley-new-found-glory-sticks-and-stones.jpg",
             altText: "a red toy poodle named Barkley sitting in the grass with New Found Glory's Stick and Stones CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "The Offspring",
@@ -112,7 +112,7 @@ const musicRecommendations = {
             url: "assets/barkley-offspring-conspiracy-of-one.jpg",
             altText: "a red toy poodle named Barkley lying on a carper with The Offspring's Conspiracy of One CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Simple Plan",
@@ -120,7 +120,7 @@ const musicRecommendations = {
             url: "assets/barkley-simple-plan-no-pads-no-helmets-just-balls.jpg",
             altText: "a red toy poodle named Barkley standing in the grass with Simple Plan's No Pads, No Helmets...Just Balls CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Sum 41",
@@ -128,7 +128,7 @@ const musicRecommendations = {
             url: "assets/barkley-sum-41-does-this-look-infected.jpg",
             altText: "a red toy poodle named Barkley sitting on a hardwood floor with Sum 41's Does This Look Infected CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
     ],
     metal: [
@@ -138,7 +138,7 @@ const musicRecommendations = {
             url: "assets/barkley-alexisonfire-crisis.jpg",
             altText: "a red toy poodle named Barkley lying in the grass with Alexisonfire's Crisis CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Intervals",
@@ -146,7 +146,7 @@ const musicRecommendations = {
             url: "assets/barkley-intervals-in-time.jpg",
             altText: "a red toy poodle named Barkley sitting on a bench with Intervals' In Time CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Lamb of God",
@@ -154,7 +154,7 @@ const musicRecommendations = {
             url: "assets/barkley-lamb-of-god-sacrament.jpg",
             altText: "a red toy poodle named Barkley lying on a chair with Lamb of God's Sacrament CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Opeth",
@@ -162,7 +162,7 @@ const musicRecommendations = {
             url: "assets/barkley-opeth-ghost-reveries.jpg",
             altText: "a red toy poodle named Barkley lying on a chair with Opeth's Ghost Reveries CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Meshuggah",
@@ -170,15 +170,15 @@ const musicRecommendations = {
             url: "assets/barkley-meshuggah-the-violent-sleep-of-reason.jpg",
             altText: "a red toy poodle named Barkley sitting on a chair with Meshuggah's The Violent Sleep of Reason CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Periphery",
-            title: "Periphery III - Select Difficulty",
+            title: "Periphery III: Select Difficulty",
             url: "assets/barkley-periphery3-select-difficulty.jpg",
             altText: "a red toy poodle named Barkley lying on a chair with Periphery's P3: Select Difficulty CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Protest the Hero",
@@ -186,7 +186,7 @@ const musicRecommendations = {
             url: "assets/barkley-protest-the-hero-scurrilous.jpg",
             altText: "a red toy poodle named Barkley lying on a couch with Protest the Hero's Scurillous CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         },
         {
             artist: "Slipknot",
@@ -194,11 +194,13 @@ const musicRecommendations = {
             url: "assets/barkley-slipknot-iowa.jpg",
             altText: "a red toy poodle named Barkley lying on a couch with Slipknot's Iowa CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet! Aut, nam temporibus, maiores aliquid dignissimos sed quis numquam earum ut reprehenderit corporis commodi consequatur. Ad?"
+            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
         }
     ]
 }
 
+
+/* variables */
 let genre;
 let genreChoice;
 let answer;
@@ -211,30 +213,34 @@ function randomMusicRecommendation(optionsArray) {
     $('main').addClass('disappear');
     $('footer').removeClass('disappear');
 
+    /* appends (or prepends) the music recommnendation info to the footer */
     $('footer .results-title')
         .append(`<h2>${finalPick.artist} - ${finalPick.title}</h2>`);
     $('footer .results-pic')
-        .append(`<div class="final-pick-image ${finalPick.orientation}">
+        .append(`<div class="final-pick-image">
                 <img src="${finalPick.url}" alt="${finalPick.altText}">
                 <div>`);
     $('footer .results-info')
-        .prepend(`<p>${finalPick.info}</p>`);
+        .prepend(`<div class="results-info-text"><p>${finalPick.info}</p><p>Check out a song <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">here</a>!</p></div>`);
 }
 
+/* hides the header, reveals first question */
 const headerListener = function() {
-    $('header input').on('click', function(event) {           //the button on the first page
+    $('header input').on('click', function(event) {
         event.preventDefault();
-        $('header').addClass('disappear');                    //hides header section
-        $('main').removeClass('disappear');                   //displays first question
+        $('header').addClass('disappear');
+        $('main').removeClass('disappear');
     });
 };
 
+/* hides the warning text once a selection is made for radio buttons */
 const warningReset = function(alertClass) {
     $('input').on('click', function () {
         $(alertClass).addClass('disappear');
     });
 }
 
+/* refreshes the page back to the start */
 const refresh = function() {
     $('footer input').on('click', function() {
         location.reload(true);
@@ -247,6 +253,7 @@ $(function () {
     headerListener();
     warningReset('.alert');
 
+    /* event listener for question 1 */
     $('.question1 form').on('submit', function(event) {
         event.preventDefault();
 
@@ -265,6 +272,7 @@ $(function () {
 
     warningReset('.alert2');
 
+    /* event listener for question 2 */
     $('.question2 form').on('submit', function(event) {
         event.preventDefault();
 
