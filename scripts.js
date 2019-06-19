f72365124066c7e1d25f27953e11e0b0e1b569ea
julienbigras <julien.bigras@hotmail.com>
2019-06-19 00:13:09 -0400

    fixed the glitchiness, added a favicon, added a unique youtube link and info section to each result

--- scripts.js
+++ scripts.js
@@ -6,7 +6,8 @@ const musicRecommendations = {
             url: "assets/barkley-billy-talent-self-titled.jpg",
             altText: "a red toy poodle named Barkley lying on a couch with Billy Talent's self-titled CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2003 debut album by the Canadian quartet features the hit songs 'Try Honesty', 'River Below', and 'Nothing to Lose'.",
+            youtube: "https://www.youtube.com/watch?v=gPCMx8yS7c0"
         },
         {
             artist: "Foo Fighters",
@@ -14,7 +15,8 @@ const musicRecommendations = {
             url: "assets/barkley-foo-fighters-the-color-and-the-shape.jpg",
             altText: "a red toy poodle named Barkley lying on a couch with the Foo Fighters' The Color and the Shape CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 1997 album by the American rock band features the hit songs 'Everlong', 'Monkey Wrench', and 'My Hero'.",
+            youtube: "https://www.youtube.com/watch?v=eBG7P-K-r1Y"
         },
         {
             artist: "Finger Eleven",
@@ -22,7 +24,8 @@ const musicRecommendations = {
             url: "assets/barkley-finger-eleven-five-crooked-lines.jpg",
             altText: "a red toy poodle named Barkley sitting on a chair with Finger Eleven's Five Crooked Lines CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2015 album by the Canadian quintet features the hit songs 'Gods of Speed', ' Criminal', and 'Wolves and Doors'.",
+            youtube: "https://www.youtube.com/watch?v=UjNR_JMaVNc"
         },
         {
             artist: "Incubus",
@@ -30,7 +33,8 @@ const musicRecommendations = {
             url: "assets/barkley-incubus-make-yourself.jpg",
             altText: "a red toy poodle named Barkley lying on a bed with Incubus' Make Yourself CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 1999 album by the California-based quintet features the hit songs 'Stellar', 'Drive', and 'Pardon Me'.",
+            youtube: "https://www.youtube.com/watch?v=PXzuDXZwZtI"
         },
         {
             artist: "Our Lady Peace",
@@ -38,7 +42,8 @@ const musicRecommendations = {
             url: "assets/barkley-our-lady-peace-happiness.jpg",
             altText: "a red toy poodle named Barkley lying on a hardwood floor with Our Lady Peace's Happiness CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 1999 album by the Canadian quartet features the hit songs 'One Man Army', 'Is Anybody Home?', and 'Thief'.",
+            youtube: "https://www.youtube.com/watch?v=I7R-PSvvziU"
         },
         {
             artist: "Matthew Good Band",
@@ -46,7 +51,8 @@ const musicRecommendations = {
             url: "assets/barkley-matthew-good-band-beautiful-midnight.jpg",
             altText: "a red toy poodle named Barkley lying on a carpet with Matthew Good Band's Beautiful Midnight CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 1999 album features the hit songs 'Hello Time Bomb', 'Strange Days', and 'Load Me Up', and won the Juno Award for Best Rock album in 2000.",
+            youtube: "https://www.youtube.com/watch?v=2xOplqgcg3g"
         },
         {
             artist: "Queens of the Stone Age",
@@ -54,7 +60,8 @@ const musicRecommendations = {
             url: "assets/barkley-queens-of-the-stone-age-like-clockwork.jpg",
             altText: "a red toy poodle named Barkley sitting on a couch with Queens of the Stone Age's Like Clockwork CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2013 album features the hit songs 'Smooth Sailing' and 'My God is the Sun', and features Dave Grohl playing drums on several tracks.",
+            youtube: "https://www.youtube.com/watch?v=iFca32_7YUU"
         },
         {
             artist: "Red Hot Chili Peppers",
@@ -62,7 +69,8 @@ const musicRecommendations = {
             url: "assets/barkley-red-hot-chili-peppers-by-the-way.jpg",
             altText: "a red toy poodle named Barkley lying on a chair with Red Hot Chili Peppers' By the Way CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2002 album by the American rock band features the hit songs 'By the Way', 'The Zephyr Song', and 'By the Way'.",
+            youtube: "https://www.youtube.com/watch?v=JnfyjwChuNU"
         },
     ],
     popPunk: [
@@ -72,7 +80,8 @@ const musicRecommendations = {
             url: "assets/barkley-blink-182-take-off-your-pants-and-jacket.jpg",
             altText: "a red toy poodle named Barkley sitting on a hardwood floor with Blink 182's Take Off Your Pants and Jacket CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2001 album by the California-based trio features the hit songs 'The Rock Show', 'First Date', and 'Stay Together For the Kids'.",
+            youtube: "https://www.youtube.com/watch?v=z7hhDINyBP0"
         },
         {
             artist: "Four Year Strong",
@@ -80,7 +89,8 @@ const musicRecommendations = {
             url: "assets/barkley-four-year-strong-explains-it-all.jpg",
             altText: "a red toy poodle named Barkley sitting on a chair with Four Year Strong's Explains It All CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2009 album contains the band's arrangements of popular 90s songs, including Alanis Morissette's 'Ironic', Third Eye Blind's 'Semi-Charmed Life', and Tal Bachman's 'She's So High'.",
+            youtube: "https://www.youtube.com/watch?v=mMRRxyztixo"
         },
         {
             artist: "Gob",
@@ -88,7 +98,8 @@ const musicRecommendations = {
             url: "assets/barkley-gob-foot-in-mouth-disease.jpg",
             altText: "a red toy poodle named Barkley lying in the grass with Gob's Foot in Mouth CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2003 album by the Canadian quartet features the hit songs 'Oh! Ellin', 'Ming Tran', and 'Give Up the Grudge'.",
+            youtube: "https://www.youtube.com/watch?v=9FrfC6ka6Qg"
         },
         {
             artist: "Green Day",
@@ -96,7 +107,8 @@ const musicRecommendations = {
             url: "assets/barkley-green-day-american-idiot.jpg",
             altText: "a red toy poodle named Barkley lying in the grass with Green Day's American Idiot CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2004 album by the American trio features the hit songs 'American Idiot', 'Holiday', and 'Wake Me Up When September Ends', and was adapted into a popular stage musical.",
+            youtube: "https://www.youtube.com/watch?v=Ee_uujKuJMI"
         },
         {
             artist: "New Found Glory",
@@ -104,7 +116,8 @@ const musicRecommendations = {
             url: "assets/barkley-new-found-glory-sticks-and-stones.jpg",
             altText: "a red toy poodle named Barkley sitting in the grass with New Found Glory's Stick and Stones CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2002 album by the Florida-based band features the hit songs 'Understatement', 'Head On Collision', and 'My Friends Over You'.",
+            youtube: "https://www.youtube.com/watch?v=QfcLcDBII78"
         },
         {
             artist: "The Offspring",
@@ -112,7 +125,8 @@ const musicRecommendations = {
             url: "assets/barkley-offspring-conspiracy-of-one.jpg",
             altText: "a red toy poodle named Barkley lying on a carper with The Offspring's Conspiracy of One CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2000 album by the California-based band features the hit songs 'Original Prankster', 'Want You Bad', and 'Million Miles Away'.",
+            youtube: "https://www.youtube.com/watch?v=_g19fCJotPc"
         },
         {
             artist: "Simple Plan",
@@ -120,7 +134,8 @@ const musicRecommendations = {
             url: "assets/barkley-simple-plan-no-pads-no-helmets-just-balls.jpg",
             altText: "a red toy poodle named Barkley standing in the grass with Simple Plan's No Pads, No Helmets...Just Balls CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2002 debut album by the Montreal-based quintet features the hit songs 'I'm Just a Kid', 'Addicted', and 'I'd Do Anything'.",
+            youtube: "https://www.youtube.com/watch?v=H2M7zo8ZziE"
         },
         {
             artist: "Sum 41",
@@ -128,7 +143,8 @@ const musicRecommendations = {
             url: "assets/barkley-sum-41-does-this-look-infected.jpg",
             altText: "a red toy poodle named Barkley sitting on a hardwood floor with Sum 41's Does This Look Infected CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2002 album by the Ontario-based band features the hit songs 'Still Waiting', 'Over My Head', and 'The Hell Song'.",
+            youtube: "https://www.youtube.com/watch?v=42k9DHPf_S0"
         },
     ],
     metal: [
@@ -138,7 +154,8 @@ const musicRecommendations = {
             url: "assets/barkley-alexisonfire-crisis.jpg",
             altText: "a red toy poodle named Barkley lying in the grass with Alexisonfire's Crisis CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2006 album by the Ontario-based band features the hit songs 'This Could Be Anywhere in the World', 'Boiled Frogs', and 'Rough Hands'.",
+            youtube: "https://www.youtube.com/watch?v=IwOPi8uyI2M"
         },
         {
             artist: "Intervals",
@@ -146,7 +163,8 @@ const musicRecommendations = {
             url: "assets/barkley-intervals-in-time.jpg",
             altText: "a red toy poodle named Barkley sitting on a bench with Intervals' In Time CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2012 instrumental EP features the tracks 'Mata Hari', 'Momento', and 'Epiphany'.",
+            youtube: "https://www.youtube.com/watch?v=0ipv_l7KoSI"
         },
         {
             artist: "Lamb of God",
@@ -154,7 +172,8 @@ const musicRecommendations = {
             url: "assets/barkley-lamb-of-god-sacrament.jpg",
             altText: "a red toy poodle named Barkley lying on a chair with Lamb of God's Sacrament CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2006 album by the Virginia-based quintet features the hit songs 'Redneck', and 'Walk With Me in Hell'.",
+            youtube: "https://www.youtube.com/watch?v=oqdZpxkzNvc"
         },
         {
             artist: "Opeth",
@@ -162,7 +181,8 @@ const musicRecommendations = {
             url: "assets/barkley-opeth-ghost-reveries.jpg",
             altText: "a red toy poodle named Barkley lying on a chair with Opeth's Ghost Reveries CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2005 album by the Swedish band features the hit song 'The Grand Conjuration', and is considered a defining album of 21st century progressive metal.",
+            youtube: "https://www.youtube.com/watch?v=2kKNT3PdDkE"
         },
         {
             artist: "Meshuggah",
@@ -170,7 +190,8 @@ const musicRecommendations = {
             url: "assets/barkley-meshuggah-the-violent-sleep-of-reason.jpg",
             altText: "a red toy poodle named Barkley sitting on a chair with Meshuggah's The Violent Sleep of Reason CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2016 album by the legendary Swedish band features the tracks 'Clockworks', and 'Born in Dissonance'.",
+            youtube: "https://www.youtube.com/watch?v=oFiDcazicdk"
         },
         {
             artist: "Periphery",
@@ -178,7 +199,8 @@ const musicRecommendations = {
             url: "assets/barkley-periphery3-select-difficulty.jpg",
             altText: "a red toy poodle named Barkley lying on a chair with Periphery's P3: Select Difficulty CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2016 album by the American band features the tracks 'Marigold', 'The Way the News Goes...', and 'Lune'.",
+            youtube: "https://www.youtube.com/watch?v=rmtU2WJfPgU"
         },
         {
             artist: "Protest the Hero",
@@ -186,7 +208,8 @@ const musicRecommendations = {
             url: "assets/barkley-protest-the-hero-scurrilous.jpg",
             altText: "a red toy poodle named Barkley lying on a couch with Protest the Hero's Scurillous CD",
             canCon: "canConYes",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2011 album by the Ontario-based band features the tracks 'C'est La Vie' and 'Hair-Trigger', and was the last album featuring the band's original lineup.",
+            youtube: "https://www.youtube.com/watch?v=TFb4gkFQOUI"
         },
         {
             artist: "Slipknot",
@@ -194,7 +217,8 @@ const musicRecommendations = {
             url: "assets/barkley-slipknot-iowa.jpg",
             altText: "a red toy poodle named Barkley lying on a couch with Slipknot's Iowa CD",
             canCon: "canConNo",
-            info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore atque quae accusantium fugiat eveniet!"
+            info: "This 2001 album by the masked American band features the hit songs 'My Plague', 'The Heretic Anthem', and the Grammy-nominated 'Left Behind'",
+            youtube: "https://www.youtube.com/watch?v=lRNYp1IiUuE"
         }
     ]
 }
@@ -221,7 +245,7 @@ function randomMusicRecommendation(optionsArray) {
                 <img src="${finalPick.url}" alt="${finalPick.altText}">
                 <div>`);
     $('footer .results-info')
-        .prepend(`<div class="results-info-text"><p>${finalPick.info}</p><p>Check out a song <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">here</a>!</p></div>`);
+        .prepend(`<div class="results-info-text"><p>${finalPick.info}</p><p>Check out a song <a href="${finalPick.youtube}" target="_blank">here</a>!</p></div>`);
 }
 
 /* hides the header, reveals first question */
